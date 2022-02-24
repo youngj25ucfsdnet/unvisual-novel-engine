a2058f6ff18c44a3a0e91dbdf01fc6fe78a9eedb
youngj25ucfsdnet <67431615+youngj25ucfsdnet@users.noreply.github.com>
2022-02-23 21:07:43 -0500

    Add files via upload

---
+++ main/unvisual novel engine/unvisual novel engine.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+	bool ifs = true;
+	string line;
+	string playername;
+
+	//player name
+	if (ifs == true) {
+		ifstream file("assets/playername.txt");
+		getline(file, playername);
+		if (playername == "") {
+			cout << "Enter your name. \n";
+			cin >> playername;
+
+			ofstream file("assets/playername.txt");
+			file << playername;
+			file.close();
+		}
+	}
+
+	//short names(abbreviations)
+	string currabv;
+	string abv1;
+	string abv2;
+	string abv3;
+	string abv4;
+	string abv5;
+	string abv6;
+	if (ifs == true) {
+		ifstream file("assets/shortnames.txt");
+		getline(file, currabv);
+		abv1 = currabv;
+		getline(file, currabv);
+		abv2 = currabv;
+		getline(file, currabv);
+		abv3 = currabv;
+		getline(file, currabv);
+		abv4 = currabv;
+		getline(file, currabv);
+		abv5 = currabv;
+		getline(file, currabv);
+		abv6 = currabv;
+	}
+
+	//names
+	string name1;
+	string name2;
+	string name3;
+	string name4;
+	string name5;
+	string name6;
+	if (ifs == true) {
+		ifstream file("assets/names.txt");
+		getline(file, currabv);
+		name1 = currabv;
+		getline(file, currabv);
+		name2 = currabv;
+		getline(file, currabv);
+		name3 = currabv;
+		getline(file, currabv);
+		name4 = currabv;
+		getline(file, currabv);
+		name5 = currabv;
+		getline(file, currabv);
+		name6 = currabv;
+	}
+
+	//game name
+	string gamename;
+	if (ifs == true) {
+		ifstream file("assets/gamename.txt");
+		getline(file, gamename);
+	}
+
+	cout << "Welcome to " << gamename << "! \npress enter to start \n";
+	//talking function
+	ifstream file("assets/dialogue.txt");
+	while (ifs == true) {
+		if (cin.get() == '\n') {
+			getline(file, line);
+			if (line == abv1) {
+				cout << name1 << ":\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == abv2) {
+				cout << name2 << ":\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == abv3) {
+				cout << name3 << ":\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == abv4) {
+				cout << name4 << ":\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == abv5) {
+				cout << name5 << ":\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == abv6) {
+				cout << name6 << ":\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == "brn") {
+				cout << "You:\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == "mc") {
+				cout << playername << "\n";
+				getline(file, line);
+				cout << line << "\n";
+			}
+			else if (line == "end") {
+				exit(1);
+			}
+			else {
+				cout << line << "\n";
+			}
+		}
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,44 @@
+visual novel but no visuals engine
+
+youre welcome >:O
+
+-- G U I D E --
+Player Name:
+- The name of the player is entered and saved in assets/playername.txt
+- If you want the name of the player to be preset and unchangable, put in your own name there.
+- To clear the player name, just delete everything inside assets/playername.txt
+- The player name cannot include spaces
+
+Names:
+- Enter up to 6 names for your characters in assets/names.txt
+- Names are put on each line
+- The short names(or abbreviations) that WILL NOT be part of the actual gameplay but are still really really important are in assets/shortnames.txt
+- Enter short characters for abbreviations  corresponding to their full names on the same numbered lines
+- Example: a is an abbrevation for apple
+- You can use the same name twice, but not the same abbreviation
+- Preset names/acronyms are brn for the main character to say something in their brain AND mc for the player name to be used
+
+Dialogue:
+- Write dialogue in assets/dialogue.txt
+- This is where you use abbreviations
+- Whoever is saying the current dialogue is determined is determined by the abbreviation and name
+Example:
+
+a
+"The quick brown fox jumps over the lazy dog."
+
+How it appears:
+Apple:
+"The quick brown fox jumps over the lazy dog."
+
+- You do not need to write :, it is already taken care of.
+- Write "end" to terminate the program function
+- If you want someone to say the name of the player, write ", mc, "(annoying i know but its the only way i know how to do it so far})
+
+Game Name:
+- Enter the name of the game in assets/gamename.txt
+- (important)
+
+-- C R E D I T S --
+James Young did all of the coding.
+If there is a bug or something contact me on Discord --> C_BCA#6095
